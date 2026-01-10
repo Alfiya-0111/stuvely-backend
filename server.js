ca3fb9fe7a0a8d820615804cf612cdac9b844d9a
khan Alfiya khatoon <102402466+kasa677@users.noreply.github.com>
2026-01-10 15:32:42 +0530

    file updated

--- server.js
+++ server.js
@@ -32,7 +32,9 @@ if (!admin.apps.length) {
     credential: admin.credential.cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  : undefined,
     }),
     databaseURL: process.env.FIREBASE_DB_URL,
   });
