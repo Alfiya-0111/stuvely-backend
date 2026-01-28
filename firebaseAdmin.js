2114716b60b69ffd03a0129cb746785ac4c23dc3
khan Alfiya khatoon <102402466+kasa677@users.noreply.github.com>
2026-01-28 16:09:23 +0530

    code updated

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,10 +1,15 @@
 import admin from "firebase-admin";
 
+// Railway ENV variable se service account uthao
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://stuvely-data-default-rtdb.firebaseio.com"
-});
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://stuvely-data-default-rtdb.firebaseio.com",
+  });
+}
 
+// ✅ YAHI EXPORT MISS THA
+export const db = admin.database();
 export default admin;
